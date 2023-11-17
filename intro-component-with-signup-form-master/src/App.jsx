66aada44cafa21ed07127acbd1da2529cdd24744
amoeba25 <shah.soham63@nmims.edu.in>
2023-11-17 15:28:54 +0530

    added desktop view

---
+++ intro-component-with-signup-form-master/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Form from './components/Form'
+import Heading from './components/Heading'
+
+function App() {
+
+  return (
+    <>
+    <div className="inner">
+      <Heading />
+      <Form /> 
+    </div>
+    </>
+  )
+}
+
+export default App
