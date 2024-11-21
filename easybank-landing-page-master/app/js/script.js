f7ebe24848dd487dd74aaaf5590a2eb1c1563c71
soham-algorizz <23sohamsh@gmail.com>
2024-11-21 13:07:02 +0530

    added recipe page

--- easybank-landing-page-master/app/js/script.js
+++ easybank-landing-page-master/app/js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+const body = document.querySelector("body");
+
+btnHamburger.addEventListener("click", () => {
+  console.log("open hamburger");
+
+  if (header.classList.contains("open")) {
+    body.classList.remove("no-scroll");
+    header.classList.remove("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add("no-scroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
